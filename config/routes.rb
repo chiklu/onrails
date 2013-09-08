17c3d83595e383e1da7b79576713a5199898dccb
chiklu <chiklu.ravi@gmail.com>
2013-09-08 20:53:03 +0530

    Reroute root to pins#index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Onrails::Application.routes.draw do
   devise_for :users
 
   get 'about'=> 'pages#about'
-  root :to => 'pages#home'
+  root :to => 'pins#index'
   
 
   # The priority is based upon order of creation:
